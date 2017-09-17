089788c3e471c91ff94e24fc685de6431e38d451
Elliot Maude <maude.elliot@gmail.com>
2017-09-16 21:28:29 -0400

    Added basic elephant search

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 //September 2017
 //------------//
 
+//NPM Requires
 const discord = require('discord.js');
+const imageSearch = require("node-google-image-search");
+
+//Custom Requires
 const config = require("./configs/config.json");
+
+
 const client = new discord.Client();
 
 // The ready event is vital, it means that your bot will only start reacting to information
@@ -17,7 +23,12 @@ client.on('ready', () => {
 client.on('message', message => {
   // If the message is "(prefix)ping"
   if(message.content.startsWith(config.prefix + "elephant")){
-  	console.log("Coming Soon!");
+  	var randomOffset = Math.floor(Math.random(0, 100));
+  	var results = imageSearch('elephant', callback, randomOffset, 1);
+
+	function callback(results) {
+		message.channel.send(results);
+	}
   }
   else if(message.content.startsWith(config.prefix + "ping")){
     // Send "pong" to the same channel
