f96baf0fe041c5d5bdcd245bc11e1d143c809c8b
Elliot Maude <maude.elliot@gmail.com>
2018-02-08 20:45:09 -0500

    Updated for running on heroku

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const discord = require('discord.js');
 //Custom Requires
 const config = require('./configs/config.json');
 const searcher = require('./imageSearcher.js');
+const express = require('express');
 
 const client = new discord.Client();
 // The ready event is vital, it means that your bot will only start reacting to information
@@ -36,4 +37,15 @@ client.on('message', message => {
 });
 
 
+//web client for heroku to bind to a port
 client.login(config.token);
+
+const app = express();
+app.all('/', (req, res) => res.send('Hello'));
+app.listen(process.env.PORT,() => console.log('express listening'));
+
+//keep alive by pinging this app on heroku
+var http = require("http");
+setInterval(function() {
+    http.get("https://elephant-bot.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
